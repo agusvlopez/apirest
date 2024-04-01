6e72aa30e3697471131f5a6c277d64eb8ed85e75
agusvlopez <agusvlopez@gmail.com>
2024-04-01 17:17:54 -0300

    add: products CRUD

---
+++ App/Controllers/ProductController.php
@@ -0,0 +1,153 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\Product;
+use App\Models\User;
+use App\Models\View;
+use App\Schema\ProductSchema;
+use App\Schema\UserSchema;
+
+class ProductController {
+    // private $userModel;
+
+    // public function __construct(User $userModel) {
+    //     $this->userModel = $userModel;
+    // }
+
+    public function getAllProducts() {
+        $productModel = new Product();
+        $products = $productModel->getAllProducts();
+
+        return View::renderJSON([
+            'status'  => "success",
+            'message' => "Productos encontrados con éxito!",
+            'users'  => $products
+        ]);
+        //echo json_encode($users);
+    }
+
+    function getProductById($params) {
+        $productModel = new Product();
+
+        if (isset($params["id"]) && $params["id"] > 0) {
+            $product = $productModel->getProduct($params["id"]);
+
+            if ($product) {
+                return View::renderJSON([
+                    'status'  => 200,
+                    'message' => "Producto encontrado con éxito!",
+                    'user'  => $product
+                ]);
+
+            } else {
+                return View::renderJSON([
+                    'status'  => 404,
+                    'message' => "Usuario no encontrado.",
+                ]);
+            }
+        } else {
+            return View::renderJSON([
+                'status'  => 400,
+                'message' => "Falta ingresar el Id del usuario.",
+            ]);
+        }
+    }
+
+    function createProduct() {
+        $productModel = new Product();
+        $postdata = file_get_contents("php://input");
+        $request = json_decode($postdata, true);
+
+        if (count($request) > 0) {
+            $product = new ProductSchema(
+                0,
+                $request["brand"],
+                $request["variant"],
+                $request["price"],
+                $request["pack_size"]
+            );
+
+            //echo json_encode($request);
+
+            $result = $productModel->createProduct($product);
+            if ($result) {
+                return View::renderJSON([
+                    'status'  => 201,
+                    'message' => "Usuario creado con éxito.",
+                ]);
+            } else {
+                return View::renderJSON([
+                    'status'  => 500,
+                    'message' => "Se produjo un error en el servidor.",
+                ]);
+            }
+        } else {
+            return View::renderJSON([
+                'status'  => 400,
+                'message' => "No se recibieron los datos correspondientes para crear el usuario.",
+            ]);
+        }  
+    }
+
+    function updateProduct($params) {
+        $productModel = new Product();
+        if (isset($params["id"]) && $params["id"] > 0) {
+            $postdata = file_get_contents("php://input");
+            
+            $request = json_decode($postdata, true);                       
+
+            if (count($request) >= 0) {
+                $product = $productModel->getProduct($params["id"]);
+                
+                $product = new ProductSchema(
+                    $params["id"],
+                    $request["brand"],
+                    $request["variant"],
+                    $request["price"],
+                    $request["pack_size"]
+                );
+                $result = $productModel->updateProduct($product, $params["id"]);
+                
+                if ($result) {
+                    return View::renderJSON([
+                        'status'  => 200,
+                        'message' => "Usuario actualizado.",
+                    ]);
+                } else {
+                    return View::renderJSON([
+                        'status'  => 500,
+                        'message' => "Ocurrió un error al crear el usuario.",
+                    ]);
+                }
+            } else {
+                return View::renderJSON([
+                    'status'  => 400,
+                    'message' => "No se recibieron los datos correspondientes para actualizar el usuario.",
+                ]);
+            }
+        } else {
+            return View::renderJSON([
+                'status'  => 400,
+                'message' => "Falta ingresar el id del usuario.",
+            ]);
+        }
+    }
+
+    function deleteProduct($params) {
+        $productModel = new Product();
+        $result = $productModel->deleteProduct($params["id"]);
+        if ($result) {
+            return View::renderJSON([
+                'status'  => 200,
+                'message' => "Producto eliminado.",
+            ]);
+        } else {
+            return View::renderJSON([
+                'status'  => 400,
+                'message' => "El producto no se ha podido eliminar. Posiblemente tenga datos relacionados.",
+            ]);
+        }
+    }
+
+    // Otros métodos para manejar otras operaciones CRUD
+}
---
+++ App/Models/Product.php
@@ -0,0 +1,85 @@
+<?php
+namespace App\Models;
+
+use PDO;
+use PDOException;
+
+class Product {
+    private $db;
+
+    public function __construct() {
+        $this->db = new PDO("mysql:host=". 'localhost' .";dbname=" . 'api_php', 'root', '');
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function getAllProducts() {
+        $stmt = $this->db->query("SELECT * FROM products");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getProduct($id) {
+        try {
+            $stmt = $this->db->query("SELECT * FROM products WHERE id = $id");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            echo "Error. " . $e->getMessage();
+            exit("Error. " . $e->getMessage());
+        }
+    }
+
+    function createProduct($product) {
+        
+        try {
+            $stmt = $this->db->prepare("INSERT INTO products (brand, variant, price, pack_size) VALUES (:brand, :variant, :price, :pack_size)");
+            $stmt->bindParam(':brand', $product->brand);
+            $stmt->bindParam(':variant', $product->variant);
+            $stmt->bindParam(':price', $product->price);
+            $stmt->bindParam(':pack_size', $product->pack_size);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            // Manejar el error de alguna manera, como registrar un mensaje de error
+            error_log("Error al crear usuario: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    function updateProduct($product, $id) {
+        
+        try {
+            $stmt = $this->db->prepare("UPDATE products SET brand = :brand, variant = :variant, price = :price, pack_size = :pack_size WHERE id = :id");
+
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':brand', $product->brand);
+            $stmt->bindParam(':variant', $product->variant);
+            $stmt->bindParam(':price', $product->price);
+            $stmt->bindParam(':pack_size', $product->pack_size);
+            $stmt->execute();
+
+            return true;
+        } catch (PDOException $e) {
+            // Manejar el error de alguna manera, como registrar un mensaje de error
+            error_log("Error al actualizar usuario: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    function deleteProduct($id) {
+        try {
+            $stmt = $this->db->prepare("DELETE FROM products WHERE id = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            
+            return true;
+
+        } catch(PDOException $e) {
+            //throw new Exception("No se ha podido conectar con la base de datos", 1);
+            error_log("Error al eliminar el usuario: " . $e->getMessage());
+            return false;
+        }
+        
+    }
+    
+    // Otros métodos CRUD según sea necesario
+}
